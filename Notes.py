5ca9fd38e176001cda2bc48df99cffdc860ce360
7vgt <33762052+7vgt@users.noreply.github.com>
2017-11-28 10:46:20 -0800

    Added previous notes
    11/27/17

--- Notes.py
+++ Notes.py
@@ -1,1 +1,32 @@
-#this is working
+print("Hello World")
+
+#Andrew Esparza
+#this is python 2.7
+
+a=4
+b=3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print()# this makes a new line. In python 3.6,it looks like: print()
+print("see if you can figure this out")
+print(14%5) # this is time 12 is the modulies #its like a mixed fraction, but the whole numbers are gone now and the denominater
+
+car_name= "weibe moble"
+car_type= "Tesla"
+car_cylinders= 8
+car_mgp=9000.1
+
+#Inline printing
+print("I have a car called the %s"% car_name)   #%s string
+print("I have a car called the %s").It's a%s" % (car_name, car_type))
+
+#asking for imput
+name = input(what is your name? ")
+print("hello %s," % name))
+Age = input("how old are you?")
+print("%s?! wow, you are really old!") % Age
