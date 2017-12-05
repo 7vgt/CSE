4ce692579f125a14677fd6a19fbd1ac712ffdae0
7vgt <33762052+7vgt@users.noreply.github.com>
2017-12-05 10:46:53 -0800

    still working on guessing game
    12/5/2017

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,7 +1,14 @@
 import random
-print(random.randint(0,50))
+print(random.randint(1,50))
 
 print("What Will The Number Be?")
+
+
+def answer_now():
+    my_guess = input("my guess")
+    correct_number = input("random")
+    print("%s,%s" % (my_guess,correct_number))
+
 
 
 
--- WarmUps.py
+++ WarmUps.py
@@ -5,3 +5,9 @@ def reverse_order():
     first_name = input("first name")
     last_name = input("last name")
     print("%s,%s" % (last_name, first_name))
+
+
+def add_py(name):
+  print("%s.py" %(name)) #solution 1
+  print(name+".py")  #solution 2
+
