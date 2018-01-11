a92d884f652d8ce64f34da5e98ed5f5e43cc6ab8
7vgt <33762052+7vgt@users.noreply.github.com>
2018-01-11 10:47:05 -0800

    Lucky 7's progress
    1/11/18

--- Andrew Esparza-Lucky 7s.py
+++ Andrew Esparza-Lucky 7s.py
@@ -1,55 +1,21 @@
+import random
 
 print("welcome to lucky 7's")
 
-import random
-
-
-A = dice1=(random.randint(1,6))
-B = dice2=(random.randint(1,6))
-
 money = 15
 
-if A+B == 7:
-    print("you won 7""A+b")
-elif A+B >= 7:
-    print("You Lost To bad To Sad""A+B")
+while money > 0:
 
+    dice1 = random.randint(1, 6)
 
+    dice2 = random.randint(1, 6)
 
+    if (dice1 + dice2) == 7:
+        print("you won, you win $4")
+        money += 4
 
-for num in range(15):
-    print(num - 1)
-
-
-a = 1
-while a < 10:
-    print(a)
-    a += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (dice1 + dice2) != 7:
+        money -= 1
 
 
 
