47632f48de55a76578b23c0ae5a104c7e2b7144d
7vgt <33762052+7vgt@users.noreply.github.com>
2018-01-16 10:18:08 -0800

    Lucky 7's Final Product
    1/16/18

--- Andrew Esparza-Lucky 7s.py
+++ Andrew Esparza-Lucky 7s.py
@@ -2,6 +2,12 @@ import random
 
 print("welcome to lucky 7's")
 
+print("lets get started")
+
+highest_round = 0
+
+rounds = 0
+
 money = 15
 
 while money > 0:
@@ -11,13 +17,15 @@ while money > 0:
     dice2 = random.randint(1, 6)
 
     if (dice1 + dice2) == 7:
-        print("you won, you win $4")
         money += 4
+        print("you won, you win 4 dollar")
+        rounds = rounds + 1
 
     if (dice1 + dice2) != 7:
         money -= 1
+        print("you have fallen, you loose 1 dollar")
+        rounds = rounds + 1
 
+print("You have lost all your money get outa here")
 
-
-
-
+print("You played %s rounds" % rounds)
