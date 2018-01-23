a752cd7b3ed0f918e8568cdbfd5bd061a8706265
7vgt <33762052+7vgt@users.noreply.github.com>
2018-01-23 10:46:43 -0800

    Hangman Needs Improvement
    1/23/2018

--- Andrew Esparza-HangMan.py
+++ Andrew Esparza-HangMan.py
@@ -7,17 +7,31 @@ import string
 A General Guide for HangMan
 1. Make a word Bank- 10 items
 2.pick a random item form the list
-3.hide the word (use *)
+3.
 4.Receal letters already guessed
 5.create the win condition
 
 """
+guess = 10
+
+while guess > 0:
+
+letters_guessed = []
+
+guesses_left =  10
 
 the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 word_bank = ["futuristic house", "milk and cookies", "snowy forest", "boomerang","spooky house","video games",
              "do you know the way","raindrop","jellyfish","thirtyvirus"]
+
 item = random.choice(word_bank)
+
 print(item)
 
-guess = int(input("Guess: "))
+word_bank = list(str1)
+
+if input = word_bank[1]:
+   print (word_bank[1])
+
+letters_guessed = (input("Guess: "))
---
+++ Testing file.py
@@ -0,0 +1,9 @@
+import random
+
+the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+word_bank = ["futuristic house", "milk and cookies", "snowy forest", "boomerang","spooky house","video games",
+             "do you know the way","raindrop","jellyfish","thirtyvirus"]
+
+item = random.choice(word_bank)
+print(item)
