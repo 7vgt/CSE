31662e6bdac0207d4e3aab778d4815eab5d2fd8d
7vgt <33762052+7vgt@users.noreply.github.com>
2018-01-24 10:47:02 -0800

    Hangman Getting there
    1/24/2018

--- Testing file.py
+++ Testing file.py
@@ -2,8 +2,30 @@ import random
 
 the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-word_bank = ["futuristic house", "milk and cookies", "snowy forest", "boomerang","spooky house","video games",
-             "do you know the way","raindrop","jellyfish","thirtyvirus"]
+guesses = 10
+
+star_bank = ["********** *****", "**** *** *******", "***** ******", "********* ******", "****** *****", "***** ****",
+             "** *** **** *** ***", "******** *******", "*********", "**********"]
+
+letters_guessed = ["abcdefghijklmnopqrstuvwxyz"]
+
+word_bank = ["futuristic house", "milk and cookies", "snowy forest", "boomerang effect","spooky house","video game",
+             "do you know the way","raindrop droptop","jellyfish","thirtyvirus"]
 
 item = random.choice(word_bank)
-print(item)
+
+if the_count[1] == star_bank:
+    print("You guessed the word, Congratulations YOU WIN!!!")
+
+
+if word_bank == star_bank:
+
+
+
+
+  if letters_guessed = word_bank[1]:
+
+
+while guesses > 0:
+        guess = int(input("Guess: "))
+        guesses -= 1
