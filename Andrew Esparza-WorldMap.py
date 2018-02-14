173c77f08c317637d1789238b2e3331a03a4a492
7vgt <33762052+7vgt@users.noreply.github.com>
2018-02-14 10:47:26 -0800

    Working on world_map
    2/14/2018

--- Andrew Esparza-WorldMap.py
+++ Andrew Esparza-WorldMap.py
@@ -1,5 +1,5 @@
 world_map = {
-    'Vroom': {
+    'VROOM': {
         'NAME': "Vicky's room",
         'DESCRIPTION': 'you are in a room with a bookshelf with scuff marks on the ground, a painting, and a book on '
                        'the shelf, theres a locked door north and a open door East w',
@@ -8,7 +8,7 @@ world_map = {
             'EAST': 'HALLWAY'
         }
      },
-    'Attic': {
+    'ATTIC': {
         'NAME': 'The Attic',
         'DESCRIPTION': "theres not much here just dust, webs, and a stair case that you came from that goes north",
         'PATHS': {
@@ -54,20 +54,48 @@ world_map = {
         'PATHS': {
             'WEST': 'MOMANDDADSROOM',
         }
-    }
+    },
     'GARDEN' : {
         'NAME': 'GARDEN',
         'DESCRIPTION': 'not much theres just some carrot and tomato plants',
         'PATHS' : {
             'SOUTH' : 'Outside'
         }
-    }
-    'OUTSIDE' : {
-        'Name' : 'OUTSIDE',
-        'DESCRIPTION': 'nothing but a sand pit with a treasure chest thats empty',
-        'PATHS' : {
-            'SOUTH' : 'DINNINGROOM',
-            'EAST' : 'TREEHOUSE'
+    },
+    'OUTSIDE': {
+        'Name': 'OUTSIDE',
+        'DESCRIPTION': 'nothing but a sand pit with a treasure chest that is empty',
+        'PATHS': {
+            'SOUTH': 'DINNINGROOM',
+            'EAST': 'TREEHOUSE'
+}
+    },
+    'HALLWAY': {
+        'Name': 'OUTSIDE',
+        'DESCRIPTION': 'nothing but a sand pit with a treasure chest that is empty',
+        'PATHS': {
+            'SOUTH': 'DINNINGROOM',
+            'EAST': 'TREEHOUSE'
 }
     }
 }
+
+
+
+current_node = world_map['VROOM']
+directions = ['SOUTH', 'NORTH','WEST','EAST']
+
+while True:
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print("you cannot go this way")
+    else:
+        print('command not recognized')
