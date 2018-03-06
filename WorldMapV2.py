e9f7549642f242325fa1683c275f642abdd6586e
7vgt <33762052+7vgt@users.noreply.github.com>
2018-03-06 10:47:41 -0800

    ketchup
    3/6/18

--- WorldMapV2.py
+++ WorldMapV2.py
@@ -1,5 +1,5 @@
 class Room(object):
-    def __init__(self, name, north, east, south, west, items, description):
+    def __init__(self, name, north, south, east, west, items, description):
         self.name = name
         self.north = north
         self.south = south
@@ -12,16 +12,43 @@ class Room(object):
         global current_node
         current_node = globals()[getattr(self, direction)]
 
-Vroom = Room('Vickys room', 'SECRETROOM', None, 'HALLWAY', None, None,"you are in a room with a bookshelf with scuff marks on the ground, a painting, and a book on the shelf, there is a strange small opening in the wall north and a open door East w")
+VROOM = Room('Vickys room', 'SECRETROOM', None, 'HALLWAY', None, None,"you are in a room with a bookshelf with scuff marks on the ground, a painting, and a book on the shelf, there is a strange small opening in the wall north and a open door East w")
 
-Attic = Room('The Attic', 'HALLWAY', None, None, None, None,"theres not much here just dust, webs, and a stair case that you came from that goes north")
+ATTIC = Room('The Attic', 'HALLWAY', None, None, None, None,"theres not much here just dust, webs, and a stair case that you came from that goes north")
 
-MasterRoom1 = Room('Master Room 1', 'LIVINGROOM', None, None, 'HALLWAY', None, "The room is filled with pictures of you and has posters of princesses lets hurry theres a exit West and North")
+MASTERROOM1 = Room('Master Room 1', 'LIVINGROOM', None, None, 'HALLWAY', None, "The room is filled with pictures of you and has posters of princesses lets hurry theres a exit West and North")
 
-MainLivingRoom = Room('Main Living Room','KITCHEN', 'PORCH', None, None, None,"Not much here just a couch, a firplace, and a TV, oh wait oh no don't wake up vicky shes on the couch asleep.")
+MAINLIVINGROOM = Room('Main Living Room','KITCHEN', 'PORCH', None, None, None,"Not much here just a couch, a firplace, and a TV, oh wait oh no don't wake up vicky shes on the couch asleep.")
 
-MOM/DAD = Room()
+MOM/DAD = Room('Vickys Mom And Dads Room', None, 'HALLWAY', 'MASTERBATHROOM1', None, None, "The room is filled with a bed, nice paintings, and a fur rug ")
 
+HALLWAY = Room('The Main Hallway', 'MASTERROOM1', 'ATTIC', 'MASTERROOM1', 'VROOM', None, "some  family pictures and 4 doors")
+
+HALLWAY1 = Room('Hallway 1', 'HALLWAY2', None, None, None, None,"Just Some family Pictures and random pictures.")
+
+LIVINGROOM = Room('LivingRoom', None, 'MASTERROOM1', 'KITCHEN', None, None, "There is a couch with 4 tables a smaller 1 seat recliner, and a fireplace")
+
+KITCHEN = Room()
+
+STREET = Room()
+
+PORCH = Room()
+
+YOUWIN = Room()
+
+GARAGE = Room()
+
+SECRETROOM = Room()
+
+GARDEN = Room()
+
+OUTSIDE = Room()
+
+TREEHOUSE = Room()
+
+HALLWAY2 = Room('Hallway 2', None, None, None, 'DINNINGROOM', None, )
+
+DINNINGROOM = ()
 
 #initialize rooms
 vickys_room = Room("west of house", "north_house", )
