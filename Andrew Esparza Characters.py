05ebc646eeb8551ccc7d03b8ba5816a9c91913e6
7vgt <33762052+7vgt@users.noreply.github.com>
2018-03-08 10:47:08 -0800

    Working on my features
    3/8/2018

--- Andrew Esparza Characters.py
+++ Andrew Esparza Characters.py
@@ -1,6 +1,6 @@
 class Character(object):
-    def __init__(self, name, move, inventory, abilities, health, status, physique, description, attack, take_damage,
-                 eat, regeneration, controller):
+    def __init__(self, name, move, inventory, abilities, health, status, physique, description, take_damage,
+                 eat):
         self.name = name
         self.move = move
         self.inventory = inventory
@@ -10,12 +10,39 @@ class Character(object):
         self.physique = physique
         self.description = description
         self.eat = eat
-        self.regeneration = regeneration
         self.take_damage = take_damage
 
+
+
     def talk_to_person(self):
-        global yu
+        if self.talk:
+            self.talk = True
+            print("Hi how are you")
+        else:
+            print("No response")
+
 
     def interaction(self):
-        global ye
+        if self.interaction:
+            self.interaction = True
+            print("You: how are you")
+            print("Random: Oh... good... bye")
+
+
+    def attack_punch(self):
+        if self.attack_punch:
+            self.attack_punch = False
+            print("You thought")
+            attack = health - 1
+
+
+My_Character = Character('Drew', None,'Diamond sword,64 stone blocks, and 64 god apples', 'levitation, strength, speed',
+                         100, None, 'dresses to imress', '6,6, blue eyes, bulked body,brown with blond in hair,'
+                         'mediam tan skin tone')
+
+
+
+
+
+
 
