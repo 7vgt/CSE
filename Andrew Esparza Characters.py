23b2a50409e88743273d9840119004c8747904f7
7vgt <33762052+7vgt@users.noreply.github.com>
2018-03-09 11:30:17 -0800

    finishing up
    3/9/2018

--- Andrew Esparza Characters.py
+++ Andrew Esparza Characters.py
@@ -5,12 +5,12 @@ class Character(object):
         self.move = move
         self.inventory = inventory
         self.abilities = abilities
-        self.health = health
+        self.health = 100
         self.status = status
         self.physique = physique
         self.description = description
         self.eat = eat
-        self.take_damage = take_damage
+        self.take_damage = health -1
 
 
 
@@ -33,16 +33,11 @@ class Character(object):
         if self.attack_punch:
             self.attack_punch = False
             print("You thought")
-            attack = health - 1
+            attack = self.health - 1
 
 
-My_Character = Character('Drew', None,'Diamond sword,64 stone blocks, and 64 god apples', 'levitation, strength, speed',
-                         100, None, 'dresses to imress', '6,6, blue eyes, bulked body,brown with blond in hair,'
-                         'mediam tan skin tone')
-
-
-
-
-
+My_Character = Character('Drew', None, ["Diamond sword,64 stone blocks", "64 Notch apples"], "levitation, strength"
+                         "speed", 100, None, "dresses to impress", "6,6, blue eyes, bulked body,brown with blond in "
+                         "hair, medium tan skin tone", -1, "god apple")
 
 
