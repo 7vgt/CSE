cdef05f4e4d91fa77574098160713066ee8b8432
7vgt <33762052+7vgt@users.noreply.github.com>
2018-03-16 11:33:31 -0700

    Items
    3/16/2018

--- Andrew Esparza - Inheritance.py
+++ Andrew Esparza - Inheritance.py
@@ -9,3 +9,17 @@ class Item(object):
 
     def drop(self):
         print("you dropped your item")
+        
+        
+class Food(Item):
+    def __init__(self, name, value, health):
+        super(Food, self).__init__(name, value)
+        self.health = health
+
+    def eat(self):
+        print("%s You have gain 10 health"% self.health)
+        self.health = self.health + 10
+
+    def chock(self):
+        print("%s You have lost 20 health"% self.health)
+        self.health = self.health - 20
