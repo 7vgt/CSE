dfe9df528d72e9ea8060b7d98711cf9bace9a028
7vgt <33762052+7vgt@users.noreply.github.com>
2018-03-22 10:27:43 -0700

    finished inheritance
    3/22/2018

--- Andrew Esparza - Inheritance.py
+++ Andrew Esparza - Inheritance.py
@@ -50,7 +50,7 @@ class Armour(Item):
         self.durability = 100
 
     def durability_damage(self):
-        print("%s you have lost 2 hearts"% self.durability)
+        print("%s you have lost 2 hearts" % self.durability)
         self.durability = self.durability - 2
 
 
@@ -62,70 +62,76 @@ class Weapon(Item):
         self.enemy = 10
 
     def deal_damage(self):
-        print("%s You have hit someone and taken 1 heat from them"% self.enemy)
+        print("%s You have hit someone and taken 1 heat from them" % self.enemy)
         self.enemy = self.enemy - 1
 
 
-class blocks(Item):
+class Blocks(Item):
     def __init__(self, name, value):
-         super(blocks, self).__init__(name, value)
+        super(Blocks, self).__init__(name, value)
 
-    def Place(self):
+    def place(self):
         print("You have placed a block in front of you")
 
-    def Break(self):
+    def destroy(self):
         print("You Have Broken a block in front of you")
 
 
-class draw(Item):
+class Draw(Item):
     def __init__(self, name, value):
-        super(draw, self).__init__(name, value)
+        super(Draw, self).__init__(name, value)
 
-    def Doodle(self):
+    def doodle(self):
         print("you drew something out of this world")
 
-    def Erase(self):
+    def erase(self):
         print("The paper is now empty and has nothing on it now")
 
-class Files(Item):
+
+class Character(Item):
     def __init__(self, name, value):
-        super(Files, self).__init__(name, value)
+        super(Character, self).__init__(name, value)
 
     def information(self):
         print("This has lots of random facts on it")
+
 
 class Inventory(Item):
     def __init__(self, name, value):
         super(Inventory, self).__init__(name, value)
 
-    def Hold(self):
+    def hold(self):
         print("you are holding on to your items")
+
 
 class Dishes(Item):
     def __init__(self, name, value):
         super(Dishes, self).__init__(name, value)
 
-    def Throw(self):
+    def throw(self):
         print("You shattered the Item ")
 
-class Ac_unit(Item):
-    def __init__(self, name, value):
-        super(Ac_unit, self).__init__(name, value)
 
-    def Cold(self):
+class Ac(Item):
+    def __init__(self, name, value):
+        super(Ac, self).__init__(name, value)
+
+    def cold(self):
         print("The room suddenly got cold")
 
-    def Hot(self):
+    def hot(self):
         print("The room got really hot all of a sudden")
         
+
 class Clock(Item):
     def __init__(self, name, value, time):
         super(Clock, self).__init__(name, value)
         self.time = time
         
-    def Clock_read(self):
+    def clock_read(self):
         print("You should have learn how to read time")
         
+
 class clothes(Item):
     def __init__(self, name, value, brand):
         super(clothes, self).__init__(name, value)
@@ -133,7 +139,11 @@ class clothes(Item):
         
     def wear(self):
         print("It snuggly fits you")
-        
-class Bake(Item):
-    def __init__(self):
-        super(Bake, self).__init__()
+
+
+class Radio(Item):
+    def __init__(self, name, value):
+        super(Radio, self).__init__(name, value)
+
+    def singing(self):
+        print("Have Mercuyyyy on my heartt....")
